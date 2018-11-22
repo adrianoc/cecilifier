61f756dfe3801c2dc0ab81758ad369b428414841
Adriano Carlos Verona <adriano.verona@gmail.com>
2018-11-21 21:27:50 -0500

    Use codemirror to show/edit

--- Cecilifier.Web/Program.cs
+++ Cecilifier.Web/Program.cs
@@ -19,6 +19,7 @@ namespace Cecilifier.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://0.0.0.0:5001")
                 .UseStartup<Startup>();
     }
 }
