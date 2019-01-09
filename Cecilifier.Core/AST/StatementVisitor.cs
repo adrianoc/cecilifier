92db094c080da9a7657f9e194cd6d9908220ca09
Adriano Carlos Verona <adriano.verona@gmail.com>
2019-01-09 02:49:36 -0500

    use a table to collect handler information

--- Cecilifier.Core/AST/StatementVisitor.cs
+++ Cecilifier.Core/AST/StatementVisitor.cs
@@ -54,122 +54,108 @@ namespace Cecilifier.Core.AST
 
         public override void VisitTryStatement(TryStatementSyntax node)
         {
-            var exceptionHandlerTable = new ExceptionHandlerEntry[node.Catches.Count + 1 + (node.Finally != null ? 1 : 0)];
+            var exceptionHandlerTable = new ExceptionHandlerEntry[node.Catches.Count + (node.Finally != null ? 1 : 0)];
+
             void SetTryStart(string instVar)
             {
                 Context.InstructionAdded -= SetTryStart;
                 exceptionHandlerTable[0].TryStart = instVar;
             }
+
             Context.InstructionAdded += SetTryStart;
-            
+
             node.Block.Accept(this);
-            
+
             var firstInstructionAfterTryCatchBlock = CreateCilInstruction(_ilVar, OpCodes.Nop);
             exceptionHandlerTable[exceptionHandlerTable.Length - 1].HandlerEnd = firstInstructionAfterTryCatchBlock; // sets up last handler end instruction
-            
-            using(Context.DefinitionVariables.WithCurrent(string.Empty, Context.DefinitionVariables.GetLastOf(MemberKind.Method), MemberKind.TryCatchLeaveTarget, firstInstructionAfterTryCatchBlock))
-            {
-                AddCilInstruction(_ilVar, OpCodes.Leave, firstInstructionAfterTryCatchBlock );
-               
-                for (int i =0; i < node.Catches.Count; i++)
-                {
-                    HandleCatchClause(node.Catches[i], exceptionHandlerTable, i + 1);
-                }
-    
-                HandleFinallyClause(node.Finally, exceptionHandlerTable, node.Catches.Count);
-            }
-            
-            AddCecilExpression($"{_ilVar}.Append({firstInstructionAfterTryCatchBlock});");
-        }
-        
-        private void HandleCatchClause(CatchClauseSyntax node, ExceptionHandlerEntry[] exceptionHandlerTable, int currentIndex)
-        {
-            exceptionHandlerTable[currentIndex].HandlerStart = AddCilInstruction(_ilVar, OpCodes.Pop); // pops the exception object from stack...
-            exceptionHandlerTable[currentIndex - 1].HandlerEnd = exceptionHandlerTable[currentIndex].HandlerStart;
 
-            if (currentIndex == 1)
+            AddCilInstruction(_ilVar, OpCodes.Leave, firstInstructionAfterTryCatchBlock);
+
+            for (int i = 0; i < node.Catches.Count; i++)
+            {
+                HandleCatchClause(node.Catches[i], exceptionHandlerTable, i, firstInstructionAfterTryCatchBlock);
+            }
+
+            HandleFinallyClause(node.Finally, exceptionHandlerTable);
+
+            AddCecilExpression($"{_ilVar}.Append({firstInstructionAfterTryCatchBlock});");
+
+            WriteExceptionHandlers(exceptionHandlerTable);
+        }
+
+        private void WriteExceptionHandlers(ExceptionHandlerEntry[] exceptionHandlerTable)
+        {
+            string methodVar = Context.DefinitionVariables.GetLastOf(MemberKind.Method);
+            foreach (var handlerEntry in exceptionHandlerTable)
+            {
+                AddCecilExpression($"{methodVar}.Body.ExceptionHandlers.Add(new ExceptionHandler(ExceptionHandlerType.{handlerEntry.Kind})");
+                AddCecilExpression("{");
+                if (handlerEntry.Kind == ExceptionHandlerType.Catch)
+                    AddCecilExpression($"    CatchType = {handlerEntry.CatchType},");
+
+                AddCecilExpression($"    TryStart = {handlerEntry.TryStart},");
+                AddCecilExpression($"    TryEnd = {handlerEntry.TryEnd},");
+                AddCecilExpression($"    HandlerStart = {handlerEntry.HandlerStart},");
+                AddCecilExpression($"    HandlerEnd = {handlerEntry.HandlerEnd}");
+                AddCecilExpression("});");
+            }
+        }
+
+        private void HandleCatchClause(CatchClauseSyntax node, ExceptionHandlerEntry[] exceptionHandlerTable, int currentIndex, string firstInstructionAfterTryCatchBlock)
+        {
+            exceptionHandlerTable[currentIndex].Kind = ExceptionHandlerType.Catch;
+            exceptionHandlerTable[currentIndex].HandlerStart = AddCilInstruction(_ilVar, OpCodes.Pop); // pops the exception object from stack...
+ 
+            if (currentIndex == 0)
             {
                 // The last instruction of the try block is the first instruction of the first catch block
                 exceptionHandlerTable[0].TryEnd = exceptionHandlerTable[currentIndex].HandlerStart;
             }
+            else
+            {
+                exceptionHandlerTable[currentIndex - 1].HandlerEnd = exceptionHandlerTable[currentIndex].HandlerStart;
+            }
 
             exceptionHandlerTable[currentIndex].TryStart = exceptionHandlerTable[0].TryStart;
             exceptionHandlerTable[currentIndex].TryEnd = exceptionHandlerTable[0].TryEnd;
+            exceptionHandlerTable[currentIndex].CatchType = ResolveType(node.Declaration.Type);
 
             VisitCatchClause(node);
-
-            /*var catchStart = AddCilInstruction(_ilVar, OpCodes.Pop); // pops the exception object from stack...
-            if (_tryCatchState.TryEnd == null)
-                _tryCatchState.TryEnd = catchStart;
-            
-            VisitCatchClause(node);
-
-            AddCilInstruction(_ilVar, OpCodes.Leave, Context.DefinitionVariables.GetLastOf(MemberKind.TryCatchLeaveTarget).VariableName);
-            var catchEnd= Context.DefinitionVariables.GetLastOf(MemberKind.TryCatchLeaveTarget).VariableName;
-
-            string methodVar = Context.DefinitionVariables.GetLastOf(MemberKind.Method);
-
-            AddCecilExpression($"{methodVar}.Body.ExceptionHandlers.Add(new ExceptionHandler(ExceptionHandlerType.Catch)");
-            AddCecilExpression("{");
-            AddCecilExpression($"    CatchType = {ResolveType(node.Declaration.Type)},");
-            AddCecilExpression($"    TryStart = {_tryCatchState.TryStart},");
-            AddCecilExpression($"    TryEnd = {_tryCatchState.TryEnd},");
-            AddCecilExpression($"    HandlerStart = {catchStart},");
-            AddCecilExpression($"    HandlerEnd = {catchEnd}");
-            AddCecilExpression("});");*/
+            AddCilInstruction(_ilVar, OpCodes.Leave, firstInstructionAfterTryCatchBlock);
         }
 
-        private void HandleFinallyClause(FinallyClauseSyntax node, ExceptionHandlerEntry[] exceptionHandlerTable, int finallyEntryIndex)
+        private void HandleFinallyClause(FinallyClauseSyntax node, ExceptionHandlerEntry[] exceptionHandlerTable)
         {
             if (node == null)
                 return;
 
+            var finallyEntryIndex = exceptionHandlerTable.Length - 1;
+
             exceptionHandlerTable[finallyEntryIndex].TryStart = exceptionHandlerTable[0].TryStart;
             exceptionHandlerTable[finallyEntryIndex].TryEnd = exceptionHandlerTable[0].TryEnd;
+            exceptionHandlerTable[finallyEntryIndex].Kind = ExceptionHandlerType.Finally;
 
-            var finallyStart = string.Empty;
             void SetFinallyStart(string instVar)
             {
                 Context.InstructionAdded -= SetFinallyStart;
                 exceptionHandlerTable[finallyEntryIndex].HandlerStart = instVar;
-                exceptionHandlerTable[finallyEntryIndex - 1].HandlerEnd = instVar;
 
-                if (finallyEntryIndex == 1)
+                if (finallyEntryIndex == 0)
                 {
                     // We have only try/finally blocks, so the end of the try (index 0) is the first instruction of the *finally*
                     exceptionHandlerTable[0].TryEnd = instVar;
+                }
+                else
+                {
+                    exceptionHandlerTable[finallyEntryIndex - 1].HandlerEnd = instVar;
                 }
             }
             Context.InstructionAdded += SetFinallyStart;
             
             base.VisitFinallyClause(node);
-            AddCilInstruction(_ilVar, OpCodes.Leave, Context.DefinitionVariables.GetLastOf(MemberKind.TryCatchLeaveTarget).VariableName);
+            AddCilInstruction(_ilVar, OpCodes.Endfinally);
         }
         
-//        public override void VisitFinallyClause(FinallyClauseSyntax node)
-//        {
-//            var finallyStart = string.Empty;
-//            void SetFinallyStart(string instVar)
-//            {
-//                Context.InstructionAdded -= SetFinallyStart;
-//                finallyStart = instVar;
-//            }
-//            Context.InstructionAdded += SetFinallyStart;
-//            
-//            base.VisitFinallyClause(node);
-//            AddCilInstruction(_ilVar, OpCodes.Leave, Context.DefinitionVariables.GetLastOf(MemberKind.TryCatchLeaveTarget).VariableName);
-//            var finallyEnd= AddCilInstruction(_ilVar, OpCodes.Nop);
-//            
-//            var methodVar = Context.DefinitionVariables.GetLastOf(MemberKind.Method).VariableName;
-//            AddCecilExpression($"{methodVar}.Body.ExceptionHandlers.Add(new ExceptionHandler(ExceptionHandlerType.Finally)");
-//            AddCecilExpression("{");
-//            AddCecilExpression($"    TryStart = {_tryCatchState.TryStart},");
-//            AddCecilExpression($"    TryEnd = {_tryCatchState.TryEnd},");
-//            AddCecilExpression($"    HandlerStart = {finallyStart},");
-//            AddCecilExpression($"    HandlerEnd = {finallyEnd}");
-//            AddCecilExpression("});");
-//        }
-//        
         struct ExceptionHandlerEntry
         {
             public ExceptionHandlerType Kind;
@@ -202,23 +188,5 @@ namespace Cecilifier.Core.AST
         }
 
         private static string _ilVar;
-        private TryCatchState _tryCatchState;
     }
-    
-    struct TryCatchState
-    {
-        public string TryStart;
-        public string TryEnd
-        {
-            get => _tryEnd;
-            set 
-            { 
-                CatchStart = value;
-                _tryEnd = value;
-            }
-        }
-        public string CatchStart { get; set; }
-
-        private string _tryEnd;
-    }    
 }
