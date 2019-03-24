17f7304fa543261ce4237f1548da3590f4ac2a7a
Adriano Carlos Verona <adriano.verona@gmail.com>
2019-03-24 02:41:16 -0400

    support for consuming generic static methods

---
+++ Cecilifier.Core.Tests/Tests/Integration/GenericsTestCase.cs
@@ -0,0 +1,26 @@
+using System;
+using NUnit.Framework;
+
+namespace Cecilifier.Core.Tests.Integration
+{
+    public class GenericsTestCase : IntegrationResourceBasedTest
+    {
+        [Test, Ignore("Requires Support for Generic Types")]
+        public void TestGenericInstanceMethods()
+        {
+            AssertResourceTest(@"Generics/InstanceMethods");   
+        }
+        
+        [Test]
+        public void TestGenericInferredStaticMethods()
+        {
+            AssertResourceTest(@"Generics/StaticInferredMethods");   
+        }
+        
+        [Test]
+        public void TestGenericExplicitStaticMethods()
+        {
+            AssertResourceTest(@"Generics/StaticExplicitMethods");   
+        }
+    }
+}
