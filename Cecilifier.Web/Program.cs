ac2c8bd35387f8fcb8f516039c969039b5713c5d
Adriano Carlos Verona <adriano.verona@gmail.com>
2019-07-03 00:39:37 -0400

    enable SSL on port 8081

--- Cecilifier.Web/Program.cs
+++ Cecilifier.Web/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace Cecilifier.Web
 {
@@ -19,8 +12,7 @@ namespace Cecilifier.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://0.0.0.0:8080")
-                //.UseUrls("http://0.0.0.0:8080", "https://0.0.0.0:8081")
+                .UseUrls("https://0.0.0.0:8081")
                 .UseStartup<Startup>();
     }
 }
