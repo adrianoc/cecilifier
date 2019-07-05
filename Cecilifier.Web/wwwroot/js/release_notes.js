eddbd4e83ef7cf06d8e84b570ee1ef89c6468b73
Adriano Carlos Verona <adriano.verona@gmail.com>
2019-07-04 23:27:18 -0400

    Remove sameSite restriction from cookie
    With that restriction we fail to retrieve the cookie when running website in external hosting

--- Cecilifier.Web/wwwroot/js/release_notes.js
+++ Cecilifier.Web/wwwroot/js/release_notes.js
@@ -2,7 +2,7 @@ function setCookie(cname,cvalue,exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;sameSite=strict";
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function getCookie(cname) {
