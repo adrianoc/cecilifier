60a14da29e5a1fb9c02b83662fdaed66d98f6776
Adriano Carlos Verona <adriano.verona@gmail.com>
2021-07-16 10:33:51 -0400

    allows users to configure multiple aspects of how variables are named in cecilified code (issue #22)

--- Cecilifier.Web/wwwroot/js/release_notes.js
+++ Cecilifier.Web/wwwroot/js/release_notes.js
@@ -1,20 +1,25 @@
+
+function deleteCookie(cname) {
+    setCookie(cname, 0, -1);
+}
+
 function setCookie(cname,cvalue,exdays) {
-    var d = new Date();
+    const d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    const expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/; SameSite=Strict";
 }
 
 function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
+    const name = cname + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
             c = c.substring(1);
         }
-        if (c.indexOf(name) == 0) {
+        if (c.indexOf(name) === 0) {
             return c.substring(name.length, c.length);
         }
     }
@@ -23,32 +28,32 @@ function getCookie(cname) {
 
 function showReleaseNotes() {
     getReleaseNotes(function(text) {
-        var c = getCookie("lastVersion");
-        var firstTime = c ? new Date(c) : new Date(0);
+        const c = getCookie("lastVersion");
+        const firstTime = c ? new Date(c) : new Date(0);
 
-        var json = JSON.parse(text);
-        var itemsToShow = json.filter(function(item, index, array) {
+        const json = JSON.parse(text);
+        const itemsToShow = json.filter(function (item, index, array) {
             return new Date(item.published_at) > firstTime;
         });
-        
+
         if (itemsToShow.length === 0)
             return;
 
         setCookie("lastVersion", itemsToShow[0].published_at, 1000);
-        
-        var latest = itemsToShow[0];
-        var html = `New version <a href='${latest.html_url}' target="_blank">${latest.tag_name} ${latest.name}</a> has been released on ${new Date(latest.published_at).toLocaleString()}.<br /><br />${latest.body.replace(/\r\n/g, "<br/>")}`;
 
-       document.getElementById("releaseNotesText").innerHTML = html;
-       document.getElementById("releaseNotesDiv").style.width = "100%";
-       document.getElementById("mainContent").style.visibility = "hidden";
+        const latest = itemsToShow[0];
+        const html = `New version <a href='${latest.html_url}' target="_blank">${latest.tag_name} ${latest.name}</a> has been released on ${new Date(latest.published_at).toLocaleString()}.<br /><br />${latest.body.replace(/\r\n/g, "<br/>")}`;
+        
+        document.getElementById("releaseNotesText").innerHTML = html;
+        document.getElementById("releaseNotesDiv").style.width = "100%";
+        document.getElementById("mainContent").style.visibility = "hidden";
     });
 }
 
 function getReleaseNotes(callback) {
-    var xhttp = new XMLHttpRequest();
+    const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState === 4 && this.status === 200) {
             callback( this.responseText);
         }
     };
