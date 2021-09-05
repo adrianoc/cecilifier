fbde93f3213c21423ec06bd3801bd4093bc9f004
Adriano Carlos Verona <adriano.verona@gmail.com>
2021-09-05 11:11:26 -0400

    adds support for cast operator (#88)

---
+++ Cecilifier.Core.Tests/Tests/Integration/Expressions/Casts/CastsTestCase.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+
+namespace Cecilifier.Core.Tests.Integration.Casts
+{
+    [TestFixture]
+    public class CastsTestCase : IntegrationResourceBasedTest
+    {
+        [Test]
+        public void TestPrimitiveNumericCasts([Values("int", "long", "double", "short", "byte", "char")] string source, [Values("int", "long", "double", "short", "byte", "char")] string target)
+        {
+            AssertResourceTestWithParameters("Expressions/Casts/PrimitiveNumericCasts", source, target);
+        }
+        
+        [Test]
+        public void TestReferenceCasts([Values("object", "string" )] string source, [Values("object", "string")] string target)
+        {
+            AssertResourceTestWithParameters("Expressions/Casts/ReferenceCasts", source, target);
+        }
+        
+        [Test]
+        public void TestReferenceCastsWithTypesFromSameModule([Values("Base", "Derived", "object" )] string source, [Values("Base", "Derived", "object")] string target)
+        {
+            AssertResourceTestWithParameters("Expressions/Casts/ReferenceCasts", source, target);
+        }
+        
+        [Test]
+        public void TestGenericTypeParameter([Values("object", "T" )] string source, [Values("object", "T")] string target)
+        {
+            AssertResourceTestWithParameters("Expressions/Casts/GenericTypeCasts", source, target);
+        }
+        
+        [Test]
+        public void TestGenerics([Values("Base<int>", "Derived" )] string source, [Values("Base<int>", "Derived")] string target)
+        {
+            AssertResourceTestWithParameters("Expressions/Casts/GenericTypeCasts", source, target);
+        }
+    }
+}
