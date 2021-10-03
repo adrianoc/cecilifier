70c8495f285551eb2d98a27d41f266dc0f799345
Adriano Carlos Verona <adriano.verona@gmail.com>
2021-10-03 01:57:53 -0400

    fixed bad rebase

---
+++ Cecilifier.Core.Tests/Tests/Unit/CecilifierUnitTestBase.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using Cecilifier.Core.Naming;
+using Cecilifier.Core.Tests.Framework;
+
+namespace Cecilifier.Core.Tests.Tests.Unit
+{
+    public class CecilifierUnitTestBase
+    {
+        protected static CecilifierResult RunCecilifier(string code)
+        {
+            var nameStrategy = new DefaultNameStrategy();
+            var memoryStream = new MemoryStream();
+            memoryStream.Write(System.Text.Encoding.ASCII.GetBytes(code));
+            memoryStream.Position = 0;
+        
+            return Cecilifier.Process(memoryStream, new CecilifierOptions { References = Utils.GetTrustedAssembliesPath(), Naming = nameStrategy });
+        }
+    }
+}
