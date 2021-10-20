37aa457e8eed2fd1a738aa8a80644799fa941f03
Adriano Carlos Verona <adriano.verona@gmail.com>
2021-10-20 00:30:03 -0400

    adds support for simple string interpolation

--- Cecilifier.Core/AST/ExpressionVisitor.cs
+++ Cecilifier.Core/AST/ExpressionVisitor.cs
@@ -660,12 +660,17 @@ namespace Cecilifier.Core.AST
             }
         }
 
+        public override void VisitInterpolatedStringExpression(InterpolatedStringExpressionSyntax node)
+        {
+            var visitor = InterpolatedStringVisitor.For(node, Context, ilVar, this);
+            node.Accept(visitor);
+        }
+
         public override void VisitSimpleLambdaExpression(SimpleLambdaExpressionSyntax node) => HandleLambdaExpression(node);
         public override void VisitParenthesizedLambdaExpression(ParenthesizedLambdaExpressionSyntax node) => HandleLambdaExpression(node);
         public override void VisitRangeExpression(RangeExpressionSyntax node) => LogUnsupportedSyntax(node);
         public override void VisitAwaitExpression(AwaitExpressionSyntax node) => LogUnsupportedSyntax(node);
         public override void VisitTupleExpression(TupleExpressionSyntax node) => LogUnsupportedSyntax(node);
-        public override void VisitInterpolatedStringExpression(InterpolatedStringExpressionSyntax node) => LogUnsupportedSyntax(node);
         public override void VisitIsPatternExpression(IsPatternExpressionSyntax node) => LogUnsupportedSyntax(node);
         public override void VisitThrowExpression(ThrowExpressionSyntax node) => LogUnsupportedSyntax(node);
         public override void VisitSwitchExpression(SwitchExpressionSyntax node) => LogUnsupportedSyntax(node);
