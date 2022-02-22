2e93e5882b9f0725ce08cea872cadbc04a08f288
Adriano Carlos Verona <adriano.verona@gmail.com>
2022-02-22 06:43:46 -0500

    fix compilation error after dotnet update

--- Cecilifier.Core/Constants.cs
+++ Cecilifier.Core/Constants.cs
@@ -11,11 +11,12 @@ public struct Constants
         public readonly string In = "ParameterAttributes.In";
         public readonly string Out = "ParameterAttributes.Out";
         public readonly string Optional = "ParameterAttributes.Optional";
+
+        public ParameterAttributesValues() { }
     }
     
     public struct ContextFlagsValues
     {
-
         /// <summary>
         /// In expressions such as `Index i; i = ^10` the assignment is actually a call to the Index constructor which in IL
         /// should be translated to a `load address of the value type` followed by a call to the constructor (instead of the
@@ -27,6 +28,8 @@ public struct Constants
         public readonly string RefReturn = "ref-return";
         public readonly string Fixed = "fixed";
         public readonly string InRangeExpression = "in-range-expressions";
+
+        public ContextFlagsValues() { }
     }
 }
 
