fb7dff5629a4d2ec7bd13324644207d758f31169
Adriano Carlos Verona <adriano.verona@gmail.com>
2022-12-01 07:40:56 -0500

    adds program to extract opcode docs from xmldoc

---
+++ Scripts/ExtractOpCodeDescriptions/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml;
+using System.Text;
+
+var doc = new XmlDocument();
+doc.Load("~/.nuget/packages/system.reflection.primitives/4.3.0/ref/netcore50/System.Reflection.Primitives.xml");
+
+var root = doc.DocumentElement;
+var opcodes = root.SelectNodes("members/member");
+
+var sb = new StringBuilder(@"let opCodes = [
+");
+foreach(var opcode in opcodes.OfType<XmlNode>().Where(c => c.Attributes[0].Value.Contains("OpCodes.")))
+        sb.AppendLine($"{{ name : \"{opcode.Attributes[0].Value.Substring(33)}\", description: \"{opcode.LastChild.InnerText}\" }},");
+
+sb.AppendLine("]; ");
+Console.WriteLine(sb.ToString());
