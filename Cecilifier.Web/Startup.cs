ecbb4d1efe48359044dbf2171cc4df9dd5c0217f
Adriano Carlos Verona <adriano.verona@gmail.com>
2024-02-01 22:08:22 -0500

    update generated csproj
    - Target net8.0
    - Reference Mono.Cecil 0.11.5

--- Cecilifier.Web/Startup.cs
+++ Cecilifier.Web/Startup.cs
@@ -45,10 +45,10 @@ namespace Cecilifier.Web
         private const string ProjectContents = @"<Project Sdk=""Microsoft.NET.Sdk"">
     <PropertyGroup>
         <OutputType>Exe</OutputType>
-        <TargetFramework>net7.0</TargetFramework>
+        <TargetFramework>net8.0</TargetFramework>
     </PropertyGroup>
     <ItemGroup>
-        <PackageReference Include=""Mono.Cecil"" Version=""0.11.4"" />
+        <PackageReference Include=""Mono.Cecil"" Version=""0.11.5"" />
     </ItemGroup>
 </Project>";
 
