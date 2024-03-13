62a9f3696f9b34e47899c8030d6e392165e19e15
Adriano Carlos Verona <adriano.verona@gmail.com>
2024-03-13 17:40:30 -0400

    code cleanup

--- Cecilifier.Core/AST/AssignmentVisitor.cs
+++ Cecilifier.Core/AST/AssignmentVisitor.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using Cecilifier.Core.Extensions;
 using Cecilifier.Core.Misc;
 using Cecilifier.Core.Variables;
