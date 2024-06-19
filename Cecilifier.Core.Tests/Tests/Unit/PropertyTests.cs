4fb2641b6cbebdcc4832496520ad7b6af9e558bd
Adriano Carlos Verona <adriano.verona@gmail.com>
2024-06-19 10:50:07 -0400

    adds test for #282
    Behavior was already correct most likely fixed by the refactoring that introduced PropertyGenerator type

--- Cecilifier.Core.Tests/Tests/Unit/PropertyTests.cs
+++ Cecilifier.Core.Tests/Tests/Unit/PropertyTests.cs
@@ -125,4 +125,33 @@ public class PropertyTests : CecilifierUnitTestBase
             Assert.That(cecilifiedCode, Does.Match(@"il_get_\d+.Emit\(OpCodes.Ldsfld, fld_prop_\d+\);"), "Getter field access should be static");
         });
     }
+    
+    [Test]
+    public void AccessorAccessibility_IsRespected()
+    {
+        var code = """
+                   class Foo
+                   {
+                       public int Value
+                       {
+                           private get => 1;
+                           set {  }
+                       }
+                   }
+                   """;
+
+        var result = RunCecilifier(code);
+        var cecilifiedCode = result.GeneratedCode.ReadToEnd();
+        
+        Assert.That(cecilifiedCode, Does.Match("""
+                                               //Getter
+                                               \s+var m_get_\d+ = new MethodDefinition\("get_Value", MethodAttributes.Private.+, .+TypeSystem.Int32\);
+                                               """));
+        
+        Assert.That(cecilifiedCode, Does.Match("""
+                                               // Setter
+                                               \s+var l_set_\d+ = new MethodDefinition\("set_Value", MethodAttributes.Public.+, .+TypeSystem.Void\);
+                                               """));
+    }
+
 }
