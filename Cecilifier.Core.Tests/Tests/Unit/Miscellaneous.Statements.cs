dbd20e46aebba681502ded230cbff6b9d93619c6
Adriano Carlos Verona <adriano.verona@gmail.com>
2024-10-02 21:20:16 -0400

    improves generated code for 'foreach' statements

--- Cecilifier.Core.Tests/Tests/Unit/Miscellaneous.Statements.cs
+++ Cecilifier.Core.Tests/Tests/Unit/Miscellaneous.Statements.cs
@@ -34,7 +34,7 @@ public partial class MiscellaneousStatements : CecilifierUnitTestBase
                  \s+var (lbl_fel_\d+) = il_M_7.Create\(OpCodes.Nop\);
                  \s+var nop_10 = il_M_7.Create\(OpCodes.Nop\);
                  \s+il_M_7.Append\(nop_10\);
-                 \s+//for condition
+                 \s+//for condition: j < 10
                  \s+il_M_7.Emit\(OpCodes.Ldloc, l_j_8\);
                  \s+il_M_7.Emit\(OpCodes.Ldc_I4, 10\);
                  \s+il_M_7.Emit\(OpCodes.Clt\);
