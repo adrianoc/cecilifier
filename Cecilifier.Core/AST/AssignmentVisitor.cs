c1a077660b6d5bf10a67cf55b5c7b9edd554e89d
Adriano Carlos Verona <adriano.verona@gmail.com>
2024-10-13 11:02:09 -0400

    replaced TODO regardint tuple handling with existing issue #93 (#303)

--- Cecilifier.Core/AST/AssignmentVisitor.cs
+++ Cecilifier.Core/AST/AssignmentVisitor.cs
@@ -78,8 +78,8 @@ namespace Cecilifier.Core.AST
 
         public override void VisitIdentifierName(IdentifierNameSyntax node)
         {
-            //TODO: tuple declaration with an initializer is represented as an assignment
-            //      revisit the following if/when we handle tuples
+            //T uple declaration with an initializer is represented as an assignment
+            //revisit the following when we handle tuples (https://github.com/adrianoc/cecilifier/issues/93)
             if (node.IsVar)
             {
                 return;
