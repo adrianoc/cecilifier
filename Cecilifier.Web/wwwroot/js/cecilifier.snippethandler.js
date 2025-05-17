70d5925880f02f21dfcc6c72afde2762014211e2
Adriano Carlos Verona <adriano.verona@gmail.com>
2025-05-17 12:21:41 -0400

    improves how list of saved snippets are shown

--- Cecilifier.Web/wwwroot/js/cecilifier.snippethandler.js
+++ Cecilifier.Web/wwwroot/js/cecilifier.snippethandler.js
@@ -81,23 +81,89 @@ function cecilifyFromSnippet(counter) {
     }
 }
 
-function showListOfLocallyStoredSnippets() {
+let savedSnippetSnack = null;
+function showListOfLocallyStoredSnippets(editor) {
     let snippetNames = [];
+    if (savedSnippetSnack !== null)
+    {
+        return;
+    }
+
     for(let i = 0; i < window.localStorage.length; i++) {
         let key = window.localStorage.key(i);
         if (key.startsWith(saved_snippet_prefix)) {
             let snippetName= key.substring(saved_snippet_prefix.length); 
-            snippetNames.push(`<div id="${snippetName}-to-remove" class="shortcut"><a href="#" title="Removes ${snippetName}." onclick="removeSnippet('${snippetName}');"><i class="fa-solid fa-trash-can"></i></a> <a href="${snippetName}" title="Loads ${snippetName}.">${snippetName}</a><br /></div>`);
+            snippetNames.push(
+                `<div id="${snippetName}-to-remove" 
+                        class="shortcut">
+                        <a href="${snippetName}" title="Loads ${snippetName} snippet into editor"><i class="fa-solid fa-upload"></i></a> 
+                        <a href="#" title="Removes ${snippetName} snippet." onclick="removeSnippet('${snippetName}');"><i class="fa-solid fa-trash-can"></i></a><a href="${snippetName}" title="Loads ${snippetName}snippet into editor"> ${snippetName}</a><br /></div>`);
         }
     }
     
-    SnackBar({
+    savedSnippetSnack = SnackBar({
         message: `<b>Saved snippets</b><br /><br/>${snippetNames.reduce( (acc, current) => `${acc}${current}`)}`,
         dismissible: true,
         status: "Info",
         timeout: false,
         icon: "!"
     });
+
+    const closeSavedSnippetListAction = setupShortcutToCloseSaveSnippetList(editor);
+    setupObserverToResetSavedSnippetListSnackUponClosing(closeSavedSnippetListAction);
+}
+
+function setupShortcutToCloseSaveSnippetList(editor) {
+
+    const action = editor.addAction({
+        id: "close-saved-snippet-list",
+
+        label: "Close saved snippet list",
+
+        keybindings: [monaco.KeyCode.KeyX],
+
+        // A precondition for this action.
+        precondition: null,
+
+        // A rule to evaluate on top of the precondition in order to dispatch the keybindings.
+        keybindingContext: null,
+
+        contextMenuGroupId: "navigation",
+
+        contextMenuOrder: 1.5,
+
+        run: function (ed) {
+            const temp = savedSnippetSnack;
+            savedSnippetSnack = null;
+            temp.Close();
+
+        },
+    });
+
+    return action;
+}
+
+function setupObserverToResetSavedSnippetListSnackUponClosing(closeSavedSnippetListAction) {
+    // To avoid showing multiple lists ideally we should be able to subscribe to a `closing` event on the snack with the list of saved snippets
+    // and set `savedSnippetSnack` to null; unfortunately there's no such event whence we rely on implementation details of SnackBar and 
+    // watch for changes in the style attribute of a element of the 'js-snackbar__wrapper' which SnackBar updates when the snack is closed.
+    const snackMessage = Array.from(document.getElementsByClassName("js-snackbar__wrapper")).filter(element => element.innerHTML.indexOf("Saved snippets") !== -1).at(0);
+
+    const config = { attributes: true, childList: false, characterData: false };
+
+    const callback = mutations => {
+        mutations.forEach(mutation => {
+            if (mutation.attributeName === 'style') {
+                savedSnippetSnack = null;
+                closeSavedSnippetListAction.dispose();
+            }
+            if (mutation.attributeName === 'style')
+                savedSnippetSnack = null;
+        });
+    };
+
+    const observer = new MutationObserver(callback);
+    observer.observe(snackMessage, config);
 }
 
 function removeSnippet(snippetName) {
