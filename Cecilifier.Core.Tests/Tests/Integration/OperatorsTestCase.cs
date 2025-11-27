736c38ca07ca42d11aa49231ffc19bbd63ea641e
Adriano Carlos Verona <adriano.verona@gmail.com>
2025-11-27 17:54:03 -0500

    reenables integration operators tests already working on SRM (#358)

--- Cecilifier.Core.Tests/Tests/Integration/OperatorsTestCase.cs
+++ Cecilifier.Core.Tests/Tests/Integration/OperatorsTestCase.cs
@@ -1,4 +1,5 @@
 using Cecilifier.ApiDriver.MonoCecil;
+using Cecilifier.ApiDriver.SystemReflectionMetadata;
 using Cecilifier.Core.AST;
 using Cecilifier.Core.Tests.Framework;
 using NUnit.Framework;
@@ -6,6 +7,7 @@ using NUnit.Framework;
 namespace Cecilifier.Core.Tests.Integration
 {
     [TestFixture(typeof(MonoCecilContext))]
+    [TestFixture(typeof(SystemReflectionMetadataContext))]
     public class Operators<TResource> : ResourceTestBase<TResource> where TResource : IVisitorContext
     {
         [Test]
