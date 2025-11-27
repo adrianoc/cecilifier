1c422aa48b8719a23128811244214f7154a4c7b2
Adriano Carlos Verona <adriano.verona@gmail.com>
2025-11-27 18:04:10 -0500

    reenables integration value type tests already working on SRM (#358)

--- Cecilifier.Core.Tests/Tests/Integration/ValueTypesTests.cs
+++ Cecilifier.Core.Tests/Tests/Integration/ValueTypesTests.cs
@@ -1,11 +1,14 @@
 using Cecilifier.ApiDriver.MonoCecil;
+using Cecilifier.ApiDriver.SystemReflectionMetadata;
 using Cecilifier.Core.AST;
 using Cecilifier.Core.Tests.Framework;
+using Cecilifier.Core.Tests.Framework.Attributes;
 using NUnit.Framework;
 
 namespace Cecilifier.Core.Tests.Integration
 {
     [TestFixture(typeof(MonoCecilContext))]
+    [TestFixture(typeof(SystemReflectionMetadataContext))]
     public class ValueTypesTests<TResource> : ResourceTestBase<TResource> where TResource : IVisitorContext
     {
         [TestCase("ValueTypeReturnAsTargetOfCall")]
@@ -15,6 +18,13 @@ namespace Cecilifier.Core.Tests.Integration
         [TestCase("SingleBoolLiteralAsTargetOfCall")]
         [TestCase("ValueTypeReturnAsTargetOfCallInsideBaseConstructorInvocation")]
         [TestCase("ValueTypeReturnAsTargetOfCallInsideConstructor")]
+        [ParameterizedResourceFilter<SystemReflectionMetadataContext>(
+            "MultipleLiteralAsTargetOfCall",
+            "SingleLiteralAsTargetOfCall",
+            "SingleDoubleLiteralAsTargetOfCall",
+            "SingleBoolLiteralAsTargetOfCall",
+            "ValueTypeReturnAsTargetOfCallInsideBaseConstructorInvocation",
+            "ValueTypeReturnAsTargetOfCallInsideConstructor")]
         public void ValueTypeAsTargetOfCall(string testResourceBaseName)
         {
             AssertResourceTest(new CecilifyTestOptions { ResourceName = $"ValueTypes/AsTargetOfCall/{testResourceBaseName}" });
