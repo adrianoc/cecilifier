690da04b5add3c2721123df4748c4cb75f77abd8
Adriano Carlos Verona <adriano.verona@gmail.com>
2026-05-26 11:53:05 -0400

    wip enabling tests

--- Cecilifier.Core.Tests/Tests/Integration/GenericsTestCase.cs
+++ Cecilifier.Core.Tests/Tests/Integration/GenericsTestCase.cs
@@ -15,7 +15,7 @@ namespace Cecilifier.Core.Tests.Integration
         [TestCase("GenericOuterNonGenericInner")]
         [TestCase("GenericOuterSingleGenericInner")]
         [TestCase("GenericOuterDeepGenericInner")]
-        [ParameterizedResourceFilter<SystemReflectionMetadataContext>("GenericOuterNonGenericInner", "GenericOuterSingleGenericInner", "GenericOuterDeepGenericInner")]
+        //[ParameterizedResourceFilter<SystemReflectionMetadataContext>("GenericOuterNonGenericInner", "GenericOuterSingleGenericInner", "GenericOuterDeepGenericInner")]
         public void TestGenericOuterAndInnerPermutations(string testName)
         {
             AssertResourceTest(new CecilifyTestOptions
