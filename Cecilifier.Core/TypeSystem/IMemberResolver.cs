00c8d3c724e8e299159f2f62a4c930d7c6848318
Adriano Carlos Verona <adriano.verona@gmail.com>
2026-06-07 10:34:02 -0400

    doc: minor doc improvements (#364)

--- Cecilifier.Core/TypeSystem/IMemberResolver.cs
+++ Cecilifier.Core/TypeSystem/IMemberResolver.cs
@@ -6,7 +6,17 @@ namespace Cecilifier.Core.TypeSystem;
 
 public interface IMemberResolver
 {
-    // Returns an expression? that represents the resolved method
+    /// <summary>Returns an expression representing the resolve method.</summary>
+    /// <remarks>
+    /// The returned expression may be the name of a local variable that has been previously
+    /// initialized with the correct expression to be used.
+    /// Some Api Drivers (for example, Mono.Cecil) will generally return a 'real' expression,
+    /// while others (like System.Reflection.Metadata) will store the final expression in a
+    /// local variable (either to avoid noise/code duplication or due to the way it needs
+    /// to emit the code) and return the name of the local variable.
+    /// </remarks>
+    /// <param name="method"></param>
+    /// <returns>Returns an expression that represents the resolved method.</returns>
     string ResolveMethod(IMethodSymbol method);
     string ResolveMethod(string declaringTypeName, string declaringTypeVariable, string methodNameForVariableRegistration, ResolvedType returnType, IReadOnlyList<ParameterSpec> parameters, int typeParameterCountCount, MemberOptions options);
     
